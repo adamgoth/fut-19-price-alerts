8bdaf8edee263ceada4294952aa619174be168bd
Adam Goth <adamgoth@gmail.com>
2018-11-27 20:06:28 -0700

    Update URL for prices

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 async function getPrice() {
   try {
     const response = await axios.get(
-      "https://www.futbin.com/19/player/562/Aubameyang/"
+      "https://www.futbin.com/19/playerPrices?player=188567"
     );
-    console.log(response);
+    console.log(response.data["188567"].prices.ps.LCPrice);
   } catch (error) {
     console.error(error);
   }
