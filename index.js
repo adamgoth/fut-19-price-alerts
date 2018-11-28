2661ebff5d7a4ce87e4321f0878ddaf093d1bbbf
Adam Goth <adamgoth@gmail.com>
2018-11-27 20:14:24 -0700

    Add sendgrid, send test email

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const axios = require("axios");
+const sgMail = require("@sendgrid/mail");
+
+const secrets = require("./secrets");
+
+sgMail.setApiKey(secrets.sgApiKey);
+
+let priceData = "";
 
 async function getPrice() {
   try {
     const response = await axios.get(
       "https://www.futbin.com/19/playerPrices?player=188567"
     );
-    console.log(response.data["188567"].prices.ps.LCPrice);
+    priceData += ` Aubamayeng: ${response.data["188567"].prices.ps.LCPrice} `;
+    const msg = {
+      to: "adamgoth@gmail.com",
+      from: "noreply@fut19prices.com",
+      subject: "FUTBIN prices",
+      text: priceData
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent!");
+      })
+      .catch(error => {
+        console.error(error.toString());
+      });
   } catch (error) {
     console.error(error);
   }
